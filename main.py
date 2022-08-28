0d5cb5ec48e5d6c2201602fdbde8a727d9e6e685
Selfie SD <webtamil0@gmail.com>
2022-08-28 21:21:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def status(folder,message,fsize):
         result = subprocess.run(["du", "-hs", f"{folder}/"], capture_output=True, text=True)
         size = result.stdout[:-(length+2)]
         try:
-            app.edit_message_text(message.chat.id, message.id, f"__Downloaded__ : **{size} **__of__**  {fsize:.1f}M**")
+            app.edit_message_text(message.chat.id, message.id, f"╭───[ ᴅᴏᴡɴʟᴏᴀᴅ ꜱᴛᴀᴛꜱ ]──⍟\n│\n│⪼ ᴅᴏᴡɴʟᴏᴀᴅ : {size} **__of__**  {fsize:.1f}M\n│⪼ ꜱɪᴢᴇ : {fsize:.1f}M\n│\n╰─────────────────⍟")
             time.sleep(10)
         except:
             time.sleep(5)
@@ -72,7 +72,7 @@ def upstatus(statusfile,message):
         with open(statusfile,"r") as upread:
             txt = upread.read()
         try:
-            app.edit_message_text(message.chat.id, message.id, f"__Uploaded__ : **{txt}**")
+            app.edit_message_text(message.chat.id, message.id, f"╭───[ ᴜᴘʟᴏᴀᴅ ꜱᴛᴀᴛꜱ ]──⍟\n│\n│⪼ ᴜᴘʟᴏᴀᴅ : **{txt}** \n│⪼ ꜱɪᴢᴇ : {fsize:.1f}M\n│\n╰─────────────────⍟")
             time.sleep(10)
         except:
             time.sleep(5)
