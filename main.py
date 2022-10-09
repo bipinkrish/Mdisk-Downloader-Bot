295ee12bb0517e2f4ace08a2704a1e00a8665f9e
Bipin <87369440+bipinkrish@users.noreply.github.com>
2022-10-09 09:45:59 +0000

    Bug Fix, now Bot Send DDL if it can;t Download

--- main.py
+++ main.py
@@ -19,9 +19,6 @@ from split import TG_SPLIT_SIZE
 bot_token = os.environ.get("TOKEN", "") 
 api_hash = os.environ.get("HASH", "") 
 api_id = os.environ.get("ID", "")
-bot_token = os.environ.get("TOKEN", "5682419728:AAGLhKvo2JCk5hH6yCJuvNhA-NYTw26Ngns") 
-api_hash = os.environ.get("HASH", "ac6664c07855e0455095d970a98a082d") 
-api_id = os.environ.get("ID", "11223922")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)
 
 
